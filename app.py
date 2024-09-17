8efe424e2b4aaf021bd1cf0ac912929514b3d945
Robin Hwang <robinny1226@gmail.com>
2024-09-17 00:02:23 -0400

    fixed spacing

--- app.py
+++ app.py
@@ -167,5 +167,5 @@ def logout():
 
 # Run the Flask application
 if __name__ == '__main__':
-   #app.run(debug=True)
-    serve(app, host='0.0.0.0', port=5000, url_scheme='https')
+   app.run(debug=True)
+    #serve(app, host='0.0.0.0', port=5000, url_scheme='https')
